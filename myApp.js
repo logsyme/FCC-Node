04f51705a5257c11490547342fb5a6f955233498
Logan Symes <logan.symes10@gmail.com>
2022-06-29 12:23:10 -0600

    commented code to test

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
-const mySecret = process.env['MESSAGE_STYLE']
 
 console.log("Hello World")
-
+/*
 app.get("/", function(req, res) {
    // res.send('Hello Express');
     absolutePath = __dirname + "/views/index.html";
@@ -12,7 +11,7 @@ app.get("/", function(req, res) {
 
 app.use("/public", express.static(__dirname + "/public"))
 
-/*
+
 app.get("/json", function(req, res) {
     data = {
         "message": "Hello json"
