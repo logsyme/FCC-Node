896432383fe608bf2a77681291396b1dd1570c3d
Logan Symes <logan.symes10@gmail.com>
2022-06-29 16:30:13 -0600

    space in logging string

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const mySecret = process.env['MESSAGE_STYLE']
 console.log("Hello World")
 
 app.use(function(req, res, next){
-    console.log(req.method + "" + req.path + " - " + req.ip)
+    console.log(req.method + " " + req.path + " - " + req.ip)
     next()
 })
 
