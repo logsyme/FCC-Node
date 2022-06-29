f5756d3dc9c1fa93991b7f7797d7a70cb558a401
Logan Symes <logan.symes10@gmail.com>
2022-06-29 16:54:01 -0600

    Added date in json response and a bunch of semicolons...

--- myApp.js
+++ myApp.js
@@ -1,13 +1,12 @@
 let express = require('express');
 let app = express();
-const mySecret = process.env['MESSAGE_STYLE']
 
-console.log("Hello World")
+console.log("Hello World");
 
 app.use(function(req, res, next){
-    console.log(req.method + " " + req.path + " - " + req.ip)
-    next()
-})
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
 app.get("/", function(req, res) {
    // res.send('Hello Express');
@@ -15,16 +14,22 @@ app.get("/", function(req, res) {
     res.sendFile(absolutePath);
 })
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", function(req, res) {
-    process.env['MESSAGE_STYLE'] == "uppercase" ? data={"message": "HELLO JSON"} : data={"message": "Hello json"}
-    res.json(data)
-})
+    process.env['MESSAGE_STYLE'] == "uppercase" ? data={"message": "HELLO JSON"} : data={"message": "Hello json"};
+    res.json(data);
+});
 
 
-
+app.get("/now", function(req, res, next){
+    req.time = new Date.toString();
+    next();
+}, function(req, res) {
+    data = {time: req.time};
+    res.json(data);
+});
 
 
 
